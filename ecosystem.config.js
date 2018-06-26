2bf25d52da1cad70eb509e2e0c1818c2278d5106
wsq <mywsq961@gmail.com>
2018-06-26 23:26:02 +0800

    新增pm2部署配置

---
+++ ecosystem.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+	/**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+	apps: [
+		// First application
+		{
+			name: 'cshun-website',
+			script: 'npm',
+			args: 'start',
+			env: {
+				COMMON_VARIABLE: 'true'
+			},
+			env_production: {
+				NODE_ENV: 'production'
+			}
+		}
+	],
+	/**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+	deploy: {
+		production: {
+			user: 'wsq',
+			host: 'tx',
+			ref: 'origin/master',
+			repo: 'https://github.com/ymz-studio/cshunWebsite.git',
+			path: '/home/wsq/cshun-website',
+			'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
+		}
+	}
+};
