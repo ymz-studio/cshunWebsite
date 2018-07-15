2726b08a8c569716b501b5539e4679963ee64ba9
dixeran <dixeran@outlook.com>
2018-07-15 12:59:12 +0800

    编辑器控件

---
+++ components/vue-editor.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="vue-editor" class="container">
+    <div class="quill-editor" :content="value" @change="onContentChange($event)" v-quill:myQuillEditor="editorOption">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      editorOption: {
+        // some quill options
+        modules: {
+          toolbar: [
+            ["bold", "italic", "underline", "strike"],
+            ["blockquote", "code-block"],
+            [{ header: 1 }, { header: 2 }],
+            [{ list: "ordered" }, { list: "bullet" }],
+            [{ script: "sub" }, { script: "super" }],
+            [{ indent: "-1" }, { indent: "+1" }],
+            [{ direction: "rtl" }],
+            [{ size: ["small", false, "large", "huge"] }],
+            [{ header: [1, 2, 3, 4, 5, 6, false] }],
+            [{ font: [] }],
+            [{ color: [] }, { background: [] }],
+            [{ align: [] }],
+            ["clean"],
+            ["link", "image", "video"]
+          ]
+        },
+        theme: "snow"
+      }
+    };
+  },
+  props: {
+    value: {
+      //传入内容
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    onContentChange({ quill, html, text }) {
+      this.$emit("input", html);
+    }
+  }
+};
+</script>
+
