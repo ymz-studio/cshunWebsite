bcdd34aa8ca139ef624efc8c830c174b5ddc9185
dixeran <dixeran@outlook.com>
2018-07-17 11:25:36 +0800

    更新编辑器配置

--- components/vue-editor.vue
+++ components/vue-editor.vue
@@ -14,18 +14,15 @@ export default {
         modules: {
           toolbar: [
             ["bold", "italic", "underline", "strike"],
-            ["blockquote", "code-block"],
+            ["blockquote"],
             [{ header: 1 }, { header: 2 }],
             [{ list: "ordered" }, { list: "bullet" }],
-            [{ script: "sub" }, { script: "super" }],
             [{ indent: "-1" }, { indent: "+1" }],
-            [{ direction: "rtl" }],
             [{ size: ["small", false, "large", "huge"] }],
             [{ header: [1, 2, 3, 4, 5, 6, false] }],
             [{ font: [] }],
             [{ color: [] }, { background: [] }],
             [{ align: [] }],
-            ["clean"],
             ["link", "image", "video"]
           ]
         },
